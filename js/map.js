ed0e636c221399b84ca0dd0c483fa06db993caea
ecorra37 <ecorra37@gmail.com>
2014-09-24 18:17:56 -0600

    EC
    FB login added

--- js/map.js
+++ js/map.js
@@ -1,20 +1,17 @@
-function initialize()
-{
-var mapProp = {
-	center: new google.maps.LatLng(49.2804177,-122.9971808),
-	zoom:13,
-	panControl:false,
-	zoomControl:true,
-	mapTypeControl:false,
-	scaleControl:true,
-	streetViewControl:false,
-	overviewMapControl:false,
-	rotateControl:false,   
-	scrollwheel: false,
-	mapTypeId:google.maps.MapTypeId.ROADMAP
-};
-var map=new google.maps.Map(document.getElementById("googleMap")
-  ,mapProp);
+function initialize(){
+	var mapProp = {
+		center: new google.maps.LatLng(49.2804177, -122.9971808),
+		zoom:13,
+		panControl:false,
+		zoomControl:true,
+		mapTypeControl:false,
+		scaleControl:true,
+		streetViewControl:false,
+		overviewMapControl:false,
+		rotateControl:false,   
+		scrollwheel: false,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
-
 google.maps.event.addDomListener(window, 'load', initialize);
