191b299100a479a53eaf3a4d99ada3db0abe9870
ecorra37 <ecorra37@gmail.com>
2014-10-17 12:12:27 -0600

    EC
    No modifications to the code but only how it all looks.

--- crawl/src/config.php
+++ crawl/src/config.php
@@ -1,8 +1,7 @@
 <?php
-$config['callback_url']         =   'http://www.eventprobe.com/?fbTrue=true'; //   /?fbTrue=true is required.
+	$config['callback_url'] = 'http://www.eventprobe.com/?fbTrue=true'; //?fbTrue=true is required.
 
-//Facebook configuration
-$config['App_ID']      =   '861882643830735';
-$config['App_Secret']  =   'c7d8579a97d35848e7231a0291e476f6'; 
-
+	//Facebook configuration
+	$config['App_ID']     = '861882643830735';
+	$config['App_Secret'] = 'c7d8579a97d35848e7231a0291e476f6'; 
 ?>
