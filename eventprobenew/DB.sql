e9de945468cf50c2e90ee30b15f1bbaef3a49985
flake79936 <ROBERT.MADRID14@GMAIL.COM>
2014-10-22 16:00:25 -0600

    RM
    new template

---
+++ eventprobenew/DB.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE EventAdvisors;
+
+CREATE TABLE Registration(
+	id INTEGER AUTO_INCREMENT,
+	UFname CHAR(255) NOT NULL,
+	ULname CHAR(255) NOT NULL,
+	UPswd CHAR(255) NOT NULL,
+	Uemail CHAR(255) NOT NULL,
+	Uphone CHAR(15) DEFAULT 'N/A',
+	Uadmin CHAR(1) DEFAULT 0,
+	UuserName CHAR(255) NOT NULL,
+	PRIMARY KEY(id, UuserName)
+);
+
+CREATE TABLE Events(
+	Eid	INT	AUTO_INCREMENT,
+	UuserName CHAR(255) NOT NULL,
+	Evename VARCHAR(26) NOT NULL,
+	EstartDate VARCHAR(20) NOT NULL,
+	EendDate VARCHAR(20) NOT NULL,
+	Eaddress VARCHAR(255) NOT NULL,
+	Ecity VARCHAR(50) NOT NULL,
+	Estate CHAR(10) NOT NULL,
+	Ezip INT(5) NOT NULL,
+	EphoneNumber INT(10),
+	Edescription VARCHAR(26) NOT NULL,
+	Etype VARCHAR(26) NOT NULL,
+	Ewebsite VARCHAR(26) NOT NULL,
+	Ehashtag CHAR(255),
+	Efacebook CHAR(255),
+	Etwitter CHAR(255),
+	Egoogle CHAR(255),
+	Eflyer CHAR(255),
+	Eother CHAR(255),
+	Elat DECIMAL(10,6),
+	Elong DECIMAL(10,6),
+	PRIMARY KEY(Eid, UuserName)
+);
+
+
+
+USERNAME:admindev
+PASSWORD:17s_9Eyr
---
+++ eventprobenew/reg_thank_you.php
@@ -0,0 +1,58 @@
+<meta http-equiv="refresh" content="2; index.html">
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+	<head>
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+		<link href="favicon.ico" rel="shortcut icon"  />
+		<title>Thank you!</title>
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+		<meta name="format-detection" content="telephone=no" />
+		<meta name="format-detection" content="email=no" />
+		
+		<!--(Start) Style Sheets-->
+			<link rel="STYLESHEET" type="text/css" href="css/fg_membersite.css">
+			
+			<!--(Start) Provided by JetDevLLC-->
+				<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+				<link href="css/styleEdit.css"            rel="stylesheet" type="text/css" />
+				<link href="css/responsiveEdit.css"       rel="stylesheet" type="text/css" />
+				<!--[if IE 6]>
+				<style type="text/css">img, div, { behavior: url("js/iepngfix.htc") }
+				</style>
+				<![endif]-->
+			<!--(End) Provided by JetDevLLC-->
+		<!--(End) Style Sheets-->
+		
+		<!--(Start) Scripts-->
+			<script src="js/jsMethods.js"            type="text/javascript"></script>
+			
+			<!--(Start) Provided by JetDevLLC-->
+				<script src="js/jquery-1.9.0.min.js" type="text/javascript"></script>
+				<script src="js/iepngfix_tilebg.js"  type="text/javascript"></script>
+				<script src="js/scrollTo.js"         type="text/javascript"></script>
+				<script src="js/global.js"           type="text/javascript"></script>
+			<!--(End) Provided by JetDevLLC-->
+		<!--(End) Scripts-->
+		
+	</head>
+	
+	<body>
+		<div class="header-wrap">
+			<div class="header">
+			</div><!--//header-->
+		</div><!--//header-wrap-->
+
+		<div class="mobile-menu-list">
+		</div><!--//mobile-menu-list-->
+	
+		<div id='fg_membersite' align="center">
+			<h2>Thanks for registering!</h2>
+<!-- 
+			Your confirmation email is on its way.
+			Please click the link in the email to complete the registration.
+ -->
+		</div>
+	</body>
+</html>
