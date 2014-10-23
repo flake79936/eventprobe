1802adf87153e4a293e459b6d0f1b86346ae6c7a
flake79936 <ROBERT.MADRID14@GMAIL.COM>
2014-10-23 10:48:43 -0600

    RM
    list of events updated (index.php)

--- index.php
+++ index.php
@@ -86,17 +86,59 @@
         	        </div>
             <div class="today">
             	<ul>
+            	
+<!--        TESTING EVENTS              -->
+            	
+<!--       Connection      	 -->
+   <?PHP
+	require_once("./include/membersite_config.php");
+	if(isset($_POST["submitted"])){
+		$result = $fgmembersite->searchEvent();
+	}
+	
+	
+	$con = mysqli_connect('localhost', 'admindev', '17s_9Eyr', 'EventAdvisors');
+	
+	if (!$con) {
+		die('Could not connect: ' . mysqli_error($con));
+	}
+
+	mysqli_select_db($con, "EventAdvisors");
+	
+	$sql = "SELECT * FROM Events WHERE Ecity = 'el paso' ORDER BY EstartDate";
+	
+	$result = mysqli_query($con, $sql);
+
+?>         	
+            	
+<!--       End Connection      	 -->
+            	
+							<?PHP
+						$i = 0;
+						while($row = mysqli_fetch_array($result)){ 
+
+					?>            	
+            	
+            	
                 	<li>
                     	<div class="info">
                         	<div class="box">
                                 <a href="#" class="btn-cross"><img src="images/btn_cross.png" alt="Cross" /></a>
                                 <h1>Today</h1>
-                                <p>Sep 6th, 9PM</p>
-                                <h1>DJ Maxwell</h1>
+                                <p><?= $row['EstartDate'] ?></p>
+                                <h1><?= $row['Evename'] ?></h1>
                             </div>
                         </div>
                     	<img src="images/sample_today.jpg" alt="Image" />
                     </li>
+                    
+                    
+                    
+                  <?PHP $i++; } ?>  
+                    
+                    
+<!--        TESTING EVENTS              -->
+                    
                     <li>
                     	<div class="info">
                         	<div class="box">
