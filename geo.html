a637b1d791075e8c3967adc39c5afc8c08ee64b6
flake79936 <ROBERT.MADRID14@GMAIL.COM>
2014-10-25 16:24:29 -0600

    RM
    scrip to get city

---
+++ geo.html
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <script language="JavaScript" src="http://www.geoplugin.net/javascript.gp" type="text/javascript"></script>
+ </head>
+ <body>
+  <script language="Javascript"> 
+	document.write("Welcome to our visitors from "+geoplugin_city()+", "+geoplugin_countryName()); 
+  </script>
+ </body>
+</html>
---
+++ geo.php
@@ -0,0 +1,21 @@
+<html>
+ <head>
+  <script language="JavaScript" src="http://www.geoplugin.net/javascript.gp" type="text/javascript"></script>
+ </head>
+ <body>
+  <script language="Javascript"> 
+// 	document.write("Welcome to our visitors from "+geoplugin_city()+", "+geoplugin_countryName()); 
+	var city = geoplugin_city()
+
+  </script>
+  
+  <?PHP 
+  
+   $city = "<script>document.write(city)</script>";
+ 
+  echo $city;
+
+  ?>
+  
+ </body>
+</html>
