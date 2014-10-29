26f102a3e1088e2e263cd23a3856325cf2bef9b1
ecorra37 <ecorra37@gmail.com>
2014-10-29 16:26:05 -0600

    EC

--- geo.php
+++ geo.php
@@ -1,19 +1,23 @@
-<html>
- <head>
-  <script language="JavaScript" src="http://www.geoplugin.net/javascript.gp" type="text/javascript"></script>
- </head>
- <body>
-  <script language="Javascript"> 
-// 	document.write("Welcome to our visitors from "+geoplugin_city()+", "+geoplugin_countryName()); 
-	var city = geoplugin_city()
+<?PHP require_once("./include/membersite_config.php"); ?>
 
-  </script>
-  
-  <?PHP 
-  
-   $city = "<script>document.write(city)</script>";
-  echo $city;
-  ?>
-  
- </body>
+<html>
+	<head>
+		<script language="JavaScript" src="http://www.geoplugin.net/javascript.gp" type="text/javascript"></script>
+	</head>
+	<body>
+		<script language="Javascript"> 
+			document.write("Welcome to our visitors from "+geoplugin_city()+", "+geoplugin_countryName()); 
+			var city = geoplugin_city();
+			document.write("\n" + city);
+			
+			window.location.href = "./map.php?city=" + city;
+		</script>
+
+		<?PHP
+			$newcity = "";
+			$city = $newcity;
+			echo $city;
+			//$fgmembersite->RedirectToURL("./map.php?city=" . $newcity);
+		?>
+	</body>
 </html>
