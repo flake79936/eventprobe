309c448dce56919d7bfd55d8135c0711bc910002
ecorra37 <ecorra37@gmail.com>
2014-11-05 13:41:06 -0700

    EC

--- reg_thank_you.php
+++ reg_thank_you.php
@@ -1,4 +1,4 @@
-<meta http-equiv="refresh" content="2; index.html">
+<meta http-equiv="refresh" content="2; index.php">
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
@@ -49,10 +49,6 @@
 	
 		<div id='fg_membersite' align="center">
 			<h2>Thanks for registering!</h2>
-<!-- 
-			Your confirmation email is on its way.
-			Please click the link in the email to complete the registration.
- -->
 		</div>
 	</body>
 </html>
