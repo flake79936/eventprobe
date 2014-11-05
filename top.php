1ec60cd7f8e3f07d2275fde86e198ec9837f3905
ecorra37 <ecorra37@gmail.com>
2014-11-05 15:27:35 -0700

    EC

--- top.php
+++ top.php
@@ -1,23 +1,41 @@
 <?PHP
-require_once("./include/membersite_config.php");
+	require_once("./include/membersite_config.php");
 	if($fgmembersite->CheckLogin()){
 		$usrname = $fgmembersite->UsrName();  
-		}
-    
-?>            
-                  
-            <div class="logo"><img src="images/logo.jpg" alt="Logo" /></div>
-            <div class="date">
-            	<div class="box">
-                	<h1><?= date("D")?> <?= date("M") ?> <?= date("d") ?></h1>
-                    <div class="temp">75</div>
-                    <div class="clear"></div>
-                </div>
-            </div>
-            <div class="profile">
-            	<div class="user"><img src="images/sample_profile.png" alt="Profile" /></div>
-                <h2><?= $usrname?> </h2>
-                <a href="#"><img src="images/btn_arrow_down_black.png" alt="Dropdown" /></a>
-                <div class="clear"></div>
-            </div>
-            <div class="clear"></div>
+	}
+?>
+<head>
+	<meta charset='utf-8'>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="./css/styleMenu.css">
+	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+	<script src="./scripts/dropDown.js"></script>
+</head>
+<div class="logo"><img src="images/logo.jpg" alt="Logo" /></div>
+<div class="date">
+	<div class="box">
+		<h1><?= date("D")?> <?= date("M") ?> <?= date("d") ?></h1>
+		<div class="temp">75</div>
+		<div class="clear"></div>
+	</div>
+</div>
+<div class="profile">
+	<div class="user"><img src="images/sample_profile.png" alt="Profile" /></div>
+	<h2><?= $usrname?> </h2>
+	<!--Creating the drop down menu with Jquery-->
+	<div id='cssmenu'>
+		<ul>
+			<li class='active has-sub'><a href="#"><img src="images/btn_arrow_down_black.png" alt="Dropdown" /></a>
+				<ul>
+					<li><a href='#'><span>Login</span></a></li>
+					<li class='last'><a href='#'><span>logout</span></a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+	
+
+	<div class="clear"></div>
+</div>
+<div class="clear"></div>
