e669775bb9caac31e031b03863ed68df38208509
flake79936 <ROBERT.MADRID14@GMAIL.COM>
2014-11-09 15:08:57 -0700

    RM

---
+++ app.php
@@ -0,0 +1,27 @@
+        	<div class="left">
+            	<div class="user">
+                    <div class="box">
+                        <img src="images/sample_profile_01.png" alt="Image" />
+                        <h3>Kim Pomeroy</h3>
+                    </div>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit."</p>
+                    <div class="clear"></div>
+                </div>
+                <div class="user">
+                	<div class="box">
+                        <img src="images/sample_profile_02.png" alt="Image" />
+                        <h3>Eylie Edie</h3>
+                    </div>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit."</p>
+                    <div class="clear"></div>
+                </div>
+            </div>
+            <div class="right">
+            	<div class="image"><img src="images/app.png" alt="Download App" /></div>
+                <div class="mobile">
+                	<h1>Download the App</h1>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="clear"></div>
---
+++ footer.php
@@ -0,0 +1,10 @@
+            <ul class="social">
+                <li><a href="#"><img src="images/btn_google.png" alt="Google" /></a></li>
+                <li><a href="#"><img src="images/btn_twitter.png" alt="Twitter" /></a></li>
+                <li><a href="#"><img src="images/btn_fb.png" alt="Facebook" /></a></li>
+                <li><a href="#"><img src="images/btn_pin.png" alt="Pinterest" /></a></li>
+            </ul>
+            <div class="footer-logo"><img src="images/footer-logo.png" alt="Logo" /></div>
+			
+			<div class="clear"></div>
+			<?PHP echo '&copy'; echo date("Y"); ?>
