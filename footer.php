f2669e74557193f818233710ea0e5882eff1a644
flake79936 <Rob@roberts-mbp.attlocal.net>
2014-12-15 18:34:17 -0700

    RM
    link on footer fixed

--- footer.php
+++ footer.php
@@ -4,6 +4,6 @@
 	<li><a href="#"><img src="images/btn_fb.png" alt="Facebook" /></a></li>
 	<li><a href="#"><img src="images/btn_pin.png" alt="Pinterest" /></a></li>
 </ul>
-<div class="footer-logo"><img src="images/footer-logo.png" alt="Logo" /></div>
+<div class="footer-logo"><a href="./index.php"><img src="images/footer-logo.png" alt="Logo" /></a></div>
 <div class="clear"></div>
 <?PHP echo '&copy'; echo date("Y"); ?>
