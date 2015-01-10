c48b10b384f5381a65c677c86da59863781546de
ecorra37 <ecorra37@gmail.com>
2015-01-09 18:56:41 -0700

    EC

--- app.php
+++ app.php
@@ -5,10 +5,6 @@
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
 <br>
-
-<a class="twitter-timeline" href="https://twitter.com/hashtag/eventprobe" data-widget-id="545741471111200768">#eventprobe Tweets</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-		
 			<img src="images/sample_profile_01.png" alt="Image" />
 			<h3>Kim Pomeroy</h3>
 		</div>
@@ -25,7 +21,11 @@
 	</div>
 </div>
 <div class="right">
-	<div class="image"><img src="images/app.png" alt="Download App" /></div>
+	<div class="image">
+		<a class="twitter-timeline" href="https://twitter.com/hashtag/eventprobe" data-widget-id="545741471111200768">#eventprobe Tweets</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		<img src="images/app.png" alt="Download App" />
+	</div>
 	<div class="mobile">
 		<h1>Download the App</h1>
 		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
