51e0695f514124e6a6220c6db01196977eaaa25a
flake79936 <Rob@Roberts-MBP.attlocal.net>
2015-01-12 18:02:09 -0700

    RM
    removed twitter static images

--- app.php
+++ app.php
@@ -5,18 +5,18 @@
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
 <br>
-			<img src="images/sample_profile_01.png" alt="Image" />
-			<h3>Kim Pomeroy</h3>
+<!-- 			<img src="images/sample_profile_01.png" alt="Image" /> -->
+<!-- 			<h3>Kim Pomeroy</h3> -->
 		</div>
-		<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit."</p>
+<!-- 		<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit."</p> -->
 		<div class="clear"></div>
 	</div>
 	<div class="user">
 		<div class="box">
-			<img src="images/sample_profile_02.png" alt="Image" />
-			<h3>Eylie Edie</h3>
+<!-- 			<img src="images/sample_profile_02.png" alt="Image" /> -->
+<!-- 			<h3>Eylie Edie</h3> -->
 		</div>
-		<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit."</p>
+<!-- 		<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit."</p> -->
 		<div class="clear"></div>
 	</div>
 </div>
@@ -24,12 +24,14 @@
 	<div class="image">
 		<a class="twitter-timeline" href="https://twitter.com/hashtag/eventprobe" data-widget-id="545741471111200768">#eventprobe Tweets</a>
 		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-		<img src="images/app.png" alt="Download App" />
+<!-- 		<img src="images/app.png" alt="Download App" /> -->
 	</div>
+<!-- 
 	<div class="mobile">
 		<h1>Download the App</h1>
 		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 	</div>
+ -->
 	<div class="clear"></div>
 </div>
 <div class="clear"></div>
