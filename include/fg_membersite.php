cf42e6657cf85bfbe64313083da568564bd024a5
ecorra37 <ecorra37@gmail.com>
2015-01-22 14:01:06 -0700

    EC

--- include/fg_membersite.php
+++ include/fg_membersite.php
@@ -1336,6 +1336,40 @@ function upLoadUserPic(){
 		}
 		return $ret_str;
 	}
+	
+	/*uses a third party tool to search for the city based on the IP address*/
+	function getCity(){
+		$user_ip = getenv('REMOTE_ADDR');
+		$geo     = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
+		$city    = $geo["geoplugin_city"];
+		
+		return $city;
+		/*
+		$region = $geo["geoplugin_regionName"];
+		$country = $geo["geoplugin_countryName"];
+		echo "City: ".$city."<br>";
+		echo "Region: ".$region."<br>";
+		echo "Country: ".$country."<br>";
+		geoplugin_request
+		geoplugin_status
+		geoplugin_credit
+		geoplugin_city
+		geoplugin_region
+		geoplugin_areaCode
+		geoplugin_dmaCode
+		geoplugin_countryCode
+		geoplugin_countryName
+		geoplugin_continentCode
+		geoplugin_latitude
+		geoplugin_longitude
+		geoplugin_regionCode
+		geoplugin_regionName
+		geoplugin_currencyCode
+		geoplugin_currencySymbol
+		geoplugin_currencySymbol_UTF8
+		geoplugin_currencyConverter
+		*/	
+	}
 
 	/*
 	Sanitize() function removes any potential threat from the
--- map.php
+++ map.php
@@ -1,27 +1,25 @@
-<?PHP require_once("./include/membersite_config.php");
-	$city = $_GET['city'];
+<?PHP
+	require_once("./include/membersite_config.php");
+	$city = $fgmembersite->getCity();
+	//$city = $_GET['city'];
 
 	$city = (string)$city;
-	
 
-// Start the session
-session_start();
-
+	// Start the session
+	session_start();
 	
 	$_SESSION['city2'] = $city;
 
 	if(isset($_POST["submitted"])){
 		$result = $fgmembersite->searchEvent();
 	}
-include 'dbconnect.php';
+	
+	include 'dbconnect.php';
 	$today = Date("m/d/Y");
 	$sql = "SELECT * FROM Events WHERE EstartDate >= '".$today."' AND Ecity = '". $city ."'  ORDER BY EstartDate;";
-	
+
 	$result = mysqli_query($con, $sql);
-?>
-
-
-<?PHP
+	
 	$i = 0;
 	while($row = mysqli_fetch_array($result)){
 		$event = $row['Evename'];
@@ -46,83 +44,83 @@ include 'dbconnect.php';
 <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.10.1.min.js"></script>
 </head> 
 <body>
-<div id="map" style="width: 370px; height: 370px;"></div>
+	<div id="map" style="width: 370px; height: 370px;"></div>
+	<script type="text/javascript" language= "php">
+	// Define your locations: HTML content for the info window, latitude, longitude
 
-<script type="text/javascript" language= "php">
-// Define your locations: HTML content for the info window, latitude, longitude
+	var locations = <?php echo json_encode($eventArray); ?>;
 
-var locations = <?php echo json_encode($eventArray); ?>;
+	// Setup the different icons and shadows
+	var iconURLPrefix = 'http://maps.google.com/mapfiles/ms/icons/';
 
-// Setup the different icons and shadows
-var iconURLPrefix = 'http://maps.google.com/mapfiles/ms/icons/';
-
-var icons = [
-'images/favicon.png'
-]
-var icons_length = icons.length;
+	var icons = [
+	'images/favicon.png'
+	]
+	var icons_length = icons.length;
 
 
-var shadow = {
-anchor: new google.maps.Point(15,33),
-url: iconURLPrefix + 'msmarker.shadow.png'
-};
+	var shadow = {
+	anchor: new google.maps.Point(15,33),
+	url: iconURLPrefix + 'msmarker.shadow.png'
+	};
 
-var map = new google.maps.Map(document.getElementById('map'), {
-zoom: 12,
-center: new google.maps.LatLng(-50.92, 120.25),
-mapTypeId: google.maps.MapTypeId.ROADMAP,
-mapTypeControl: true,
-streetViewControl: true,
-panControl: true,
-zoomControlOptions: {
-position: google.maps.ControlPosition.LEFT_BOTTOM
-}
-});
+	var map = new google.maps.Map(document.getElementById('map'), {
+	zoom: 12,
+	center: new google.maps.LatLng(-50.92, 120.25),
+	mapTypeId: google.maps.MapTypeId.ROADMAP,
+	mapTypeControl: true,
+	streetViewControl: true,
+	panControl: true,
+	zoomControlOptions: {
+	position: google.maps.ControlPosition.LEFT_BOTTOM
+	}
+	});
 
-var infowindow = new google.maps.InfoWindow({
-maxWidth: 160
-});
+	var infowindow = new google.maps.InfoWindow({
+	maxWidth: 160
+	});
 
-var marker;
-var markers = new Array();
+	var marker;
+	var markers = new Array();
 
-var iconCounter = 0;
+	var iconCounter = 0;
 
-// Add the markers and infowindows to the map
-for (var i = 0; i < locations.length; i++) {  
-marker = new google.maps.Marker({
-position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-map: map,
-icon : icons[iconCounter],
-shadow: shadow
-});
+	// Add the markers and infowindows to the map
+	for (var i = 0; i < locations.length; i++) {  
+	marker = new google.maps.Marker({
+	position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+	map: map,
+	icon : icons[iconCounter],
+	shadow: shadow
+	});
 
-markers.push(marker);
+	markers.push(marker);
 
-google.maps.event.addListener(marker, 'click', (function(marker, i) {
-return function() {
-infowindow.setContent(locations[i][0]);
-infowindow.open(map, marker);
-}
-})(marker, i));
+	google.maps.event.addListener(marker, 'click', (function(marker, i) {
+	return function() {
+	infowindow.setContent(locations[i][0]);
+	infowindow.open(map, marker);
+	}
+	})(marker, i));
 
-iconCounter++;
-// We only have a limited number of possible icon colors, so we may have to restart the counter
-if(iconCounter >= icons_length){
-iconCounter = 0;
-}
-}
+	iconCounter++;
+	// We only have a limited number of possible icon colors, so we may have to restart the counter
+	if(iconCounter >= icons_length){
+	iconCounter = 0;
+	}
+	}
 
-function AutoCenter() {
-//  Create a new viewpoint bound
-var bounds = new google.maps.LatLngBounds();
-//  Go through each...
-$.each(markers, function (index, marker) {
-bounds.extend(marker.position);
-});
-//  Fit these bounds to the map
-map.fitBounds(bounds);
-}
-AutoCenter();
-</script>
+	function AutoCenter() {
+	//  Create a new viewpoint bound
+	var bounds = new google.maps.LatLngBounds();
+	//  Go through each...
+	$.each(markers, function (index, marker) {
+	bounds.extend(marker.position);
+	});
+	//  Fit these bounds to the map
+	map.fitBounds(bounds);
+	}
+	AutoCenter();
+	</script>
 </body>
+</html>
