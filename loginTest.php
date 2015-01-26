ca56f1073e13cb638656331c10cd9a9c11d26d2b
flake79936 <Rob@roberts-mbp.attlocal.net>
2015-01-26 13:05:06 -0700

    RM

---
+++ loginTest.php
@@ -0,0 +1,43 @@
+<link rel="stylesheet" href="./css/bootstrap.min.css">
+<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container-fluid">      
+      <div class="nav-collapse">
+        <ul class="nav">
+
+          <!-- here comes the important part -->
+ 
+           <li class="dropdown" id="menu1">
+             <a class="dropdown-toggle" data-toggle="dropdown" href="#menu1">
+               Login
+                <b class="caret"></b>
+             </a>
+             <div class="dropdown-menu">
+               <form style="margin: 0px" accept-charset="UTF-8" action="/sessions" method="post"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /><input name="authenticity_token" type="hidden" value="4L/A2ZMYkhTD3IiNDMTuB/fhPRvyCNGEsaZocUUpw40=" /></div>
+                 <fieldset class='textbox' style="padding:10px">
+                   <input style="margin-top: 8px" type="text" placeholder="Username" />
+                   <br>
+                   <input style="margin-top: 8px" type="password" placeholder="Passsword" />
+                   <input class="btn-primary" name="commit" type="submit" value="Log In" />
+                 </fieldset>
+               </form>
+             </div>
+           </li>
+ 
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+<script src="./js/jquery-1.11.1.min.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+<script language="javascript">
+    $('.dropdown-toggle').dropdown();
+    $('.dropdown-menu').find('form').click(function (e) {
+        e.stopPropagation();
+      });
+</script>
