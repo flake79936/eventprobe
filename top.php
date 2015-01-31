d877672a17a85c1c556f4e775bef3bf518d11af9
flake79936 <Rob@roberts-mbp.attlocal.net>
2015-01-31 12:33:06 -0700

    RM

--- top.php
+++ top.php
@@ -89,7 +89,7 @@ $fgmembersite->RedirectToURL("./index.php");
         <ul class="nav">
 
           <!-- here comes the important part -->
- 
+ 	<?PHP if(!$bool){ ?>
            <li class="dropdown" id="menu1">
              <a class="dropdown-toggle" data-toggle="dropdown" href="#menu1">
                Login
@@ -121,7 +121,55 @@ $fgmembersite->RedirectToURL("./index.php");
                </form>
              </div>
            </li>
- 
+           
+ 		<?PHP }  else { ?>
+ 		
+ 		           <li class="dropdown" id="menu1">
+             <a class="dropdown-toggle" data-toggle="dropdown" href="#menu1">
+               Login
+                <b class="caret"></b>
+             </a>
+             <div class="dropdown-menu">
+<!--                <form style="margin: 0px" accept-charset="UTF-8" action="/sessions" method="post"> -->
+               <form style="margin: 0px" id="login" action="<?php echo $fgmembersite->GetSelfScript(); ?>" method="POST" accept-charset="UTF-8" enctype="multipart/form-data">
+
+				<div class="login">
+
+
+					   		<div style="margin:0;padding:0;display:inline">
+						    	<input name="utf8" type="hidden" value="&#x2713;" />
+					   			<input name="authenticity_token" type="hidden" value="4L/A2ZMYkhTD3IiNDMTuB/fhPRvyCNGEsaZocUUpw40=" />
+					   		</div>
+						 <fieldset class='textbox' style="padding:10px">
+						<a role="menuitem" tabindex="-1" href='./EventCreation.php'>
+						<span>Create Event</span>
+						</a>
+                		<br>
+                 
+                 		<a role="menuitem" tabindex="-1" href='./logout.php'>
+						<span>logout</span>
+						</a>
+						 	  <!-- 
+<input style="margin-top: 8px" name="UuserName" type="text" placeholder="Username" id="UuserName"  />
+						   		<br>
+						   	  <input style="margin-top: 8px" type="password" name='UPswd' placeholder="Passsword" id='UPswd' />
+				   
+ -->
+                </div>
+<!-- 
+                   
+							   <input class="btn-primary" input id="submitButton" type="submit" name="Submit" value="Login" />
+							   <br>
+							   <a style="margin-top: 8px" href="https://www.facebook.com/dialog/oauth?client_id=861882643830735&amp;redirect_uri=http://www.eventprobe.com/?fbTrue=true">
+								<img src="./images/login-button.png" alt="Sign in with Facebook"></a>
+ -->
+                 		</fieldset>
+               </form>
+             </div>
+           </li>
+           
+ 		
+ 		 <?PHP } ?>
         </ul>
                
                
