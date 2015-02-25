a7ea15b91d773bf80f4d545e2c3b1745b5dfab91
flake79936 <Rob@roberts-mbp.attlocal.net>
2015-02-24 18:01:32 -0700

    RM

--- reg_thank_you.php
+++ reg_thank_you.php
@@ -1,4 +1,4 @@
-<!-- <meta http-equiv="refresh" content="2; index.php"> -->
+<meta http-equiv="refresh" content="2; index.php">
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
@@ -43,7 +43,7 @@
  -->
 		
 		<div id='fg_membersite' align="center">
-			<h1>Thanks for registering!</h1>
+			<h1>Thanks for Registering!</h1>
 		</div>
 		
 		<div class="links">
