fc54084d2c217780be533a7cfd2ee4930e5a2a5e
flake79936 <Rob@roberts-mbp.attlocal.net>
2015-03-05 20:27:22 -0700

    RM

---
+++ deleteEvent.php
@@ -0,0 +1,28 @@
+<?php
+// include "database_connection.php"; //Include database connection settings file
+		require_once("./include/membersite_config.php");
+include "dbconnect.php";
+	if(!$fgmembersite->CheckSession()){
+		$fgmembersite->RedirectToURL("./index.php");
+		exit;
+	}
+
+		$usrname = $fgmembersite->UsrName();
+
+// if(isset($_POST['id']) 			&& 
+// 	isset($_POST['Evename'])  &&
+// 	isset($_POST['Edescription'])   &&
+// 	isset($_POST['Efacebook'])		&& 
+// 	!empty($_POST['id']) 		&& 
+// 	!empty($_POST['Evename']) && 
+// 	!empty($_POST['Edescription'])  && 
+// 	!empty($_POST['Efacebook'])
+// 	)
+// {
+// $con = mysqli_connect('localhost', 'user', 'Xzr?f270', 'EventAdvisors');
+$newEventID = $_GET['eid'];
+
+$sql= "DELETE FROM Events  WHERE Eid = '".$newEventID."' AND UuserName = '".$usrname."' ";
+mysqli_query($con, $sql);
+
+?>
