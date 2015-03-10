00742feb628a6e23b985472390b500dd1100df61
ecorra37 <ecorra37@gmail.com>
2015-03-09 21:51:59 -0500

    EC

---
+++ date.php
@@ -0,0 +1,21 @@
+<?php
+echo "Today is " . date("m/d/Y") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l") . "<br>";
+echo "Today is " . date("D") . "<br>";
+
+
+$trimDate = substr(date("m/d/Y"), 0, 5);
+echo $trimDate . "<br>";
+
+$date = strtotime("+2 day", strtotime(date("m/d/Y")));
+echo date("Y-m-d", $date) . "<br>";
+
+echo date("G:i") . "<br>";
+
+date_default_timezone_set("America/Denver");
+echo date_default_timezone_get() . "<br>";
+
+echo date("G:i") . "<br>";
+?>
--- events.php
+++ events.php
@@ -2,39 +2,37 @@
 	<?PHP
 		require_once("./include/membersite_config.php");
 		$city = $fgmembersite->getCity();
-// 		$city="el paso";
 		if(isset($_POST["submitted"])){
 			$result = $fgmembersite->searchEvent();
 		}
 	
+		
 		include 'dbconnect.php';
 
-		$timezone = $fgmembersite->getLocalTimeZone();
-		date_default_timezone_set($timezone);
-
 		$today = Date("m/d/Y");
-		$sql = "SELECT * FROM Events WHERE EstartDate >= '".$today."' AND Ecity = '".$city."' AND Edisplay='1' ORDER BY EstartDate";
+		$sql = "SELECT * FROM Events WHERE EstartDate >= '".$today."' AND Ecity = '".$city."' ORDER BY EstartDate";
 		$result = mysqli_query($con, $sql);
 
 		$i = 0;
 		while($row = mysqli_fetch_array($result)){
-			//day name of the date	
-			$today = date("m/d/Y");
-			$dt = strtotime($row['EstartDate']);
-			$day = date("l", $dt);
-			if ($today===$row['EstartDate']){ $day = "Today"; }
-		?>
-		<a onClick="seeMoreInfo(<?= $row['Eid'] ?>);"><li>
-			<div class="info">
-				<div class="box">
-					<img src="images/btn_cross.png" alt="Cross" class="btn-cross"/>
-					<h1><?= $day ?></h1>
-					<p> <?= substr($row['EstartDate'], 0, 5);?>, <?= $row['EtimeStart'] ?></p>
-					<h3><?= $row['Evename'] ?></h3>
-				</div>
+		//day name of the date	
+		$today= date("m/d/Y");
+		$dt = strtotime($row['EstartDate']);
+		$day = date("l", $dt);
+		if ($today===$row['EstartDate']){
+		$day="Today";}
+	?>
+	<li>
+		<div class="info">
+			<div class="box">
+				<a href="#" class="btn-cross"><img src="images/btn_cross.png" alt="Cross" /></a>
+				<h1><?=$day?></h1>
+				<p> <?=substr($row['EstartDate'], 0, 5);?>, <?= $row['EtimeStart'] ?></p>
+				<h1><?= $row['Evename'] ?></h1>
 			</div>
-			<img width="200px" height="200px" src="<?= $row['Eflyer'] ?>" alt="Image" />
-		</li></a>
+		</div>
+		<img width="200px" height="200px" src="<?= $row['Eflyer'] ?>" alt="Image" />
+	</li>
 
 	<?PHP $i++; } ?>
 <div class="clear"></div>
