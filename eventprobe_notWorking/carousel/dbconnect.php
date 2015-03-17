aac9cbff6aaf1eddaa1b51d275b630c5fe0ae657
ecorra37 <ecorra37@gmail.com>
2015-03-17 17:01:30 -0500

    EC
    deleted a backup that was not working.

--- eventprobe_notWorking/carousel/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-	$con = mysqli_connect('localhost', 'admindev', '17s_9Eyr', 'EventAdvisors');
-
-	if (!$con) {
-		die('Could not connect: ' . mysqli_error($con));
-	}
-
-	mysqli_select_db($con, "EventAdvisors");
-?>
--- eventprobe_notWorking/livetableedit/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con = mysql_connect("localhost","root","");
-mysql_select_db("test",$con);
-?>
