38ac85741769fda3df6b42c05f666182854e2be9
ecorra37 <ecorra37@gmail.com>
2015-03-20 15:24:27 -0500

    EC

--- eventDisplayPage.php
+++ eventDisplayPage.php
@@ -1,8 +1,7 @@
-<!--AJAX Module-->
 <?PHP
 	require_once("./include/membersite_config.php"); 
 	$newEventID = $_GET['eid'];
-	// 	$newEventID = "35";
+	//$newEventID = "124";
 	include 'dbconnect.php';
 	
 	if(isset($_POST["submitted"])){
@@ -18,8 +17,7 @@
 	$inDBUser = $fgmembersite->getUserInDB($newEventID);
 ?>
 
-<!doctype html>
-<html>
+<html lang="en">
 	<head>
 		<meta charset="utf-8"/>
 		<title>Eventprobe</title>
@@ -68,7 +66,7 @@
 			function seeMoreInfo(str){
 				window.location = "./eventDisplayPage.php?eid="+str;
 			}
-		</script>		
+		</script>	
 
 		<script>
 			$(document).ready(function(){
@@ -85,8 +83,8 @@
 		<!--FAVICON-->
 		<link rel="shortcut icon" href="favicon.ico"  />
 	</head>
-
-	<body lang="en">
+	
+	<body>
 		<div class="search">
 			<form>
 				<input type="text" onkeyup="showHint(this.value)" placeholder="Search for Event"><br>
@@ -97,19 +95,13 @@
 				<a id="" onClick="showHint('');"><img alt="art" src="./images/clear.png"/></a>
 			</form>
 		</div>
-
+		
 		<div class="top">
 			<?PHP include './top.php';?>
 		</div>
 		
 		<div class="eventDisplayPage">
 			<form id="eventForm" action="<?php echo $fgmembersite->GetSelfScript(); ?>" method="POST" accept-charset="UTF-8" enctype="multipart/form-data">
-				<input type="hidden" name="submitted" id="submitted" value="1"/>
-				<input type="hidden" name="Eid" id="Eid" value="<?PHP echo $newEventID; ?>"/>
-						
-				<div><span class="error"><?php echo $fgmembersite->GetErrorMessage(); ?></span></div>
-				<table width="1108" border="0">
-				
 				<?PHP
 					$qry = "SELECT * FROM Events WHERE Eid = '".$newEventID."' AND Edisplay='1';";
 					$result = mysqli_query($con, $qry);
@@ -137,298 +129,162 @@
 						/** End format phone number**/
 				?>
 				
-						<tr>
-							<td width="400"><h1><?= $row['Evename'] ?></h1></td>
-							<td width="21">&nbsp;</td>
-							<td width="88"><h1><?= $row['Ehashtag'] ?></h1></td>
-							<td width="86">&nbsp;</td>
-							<td width="491">&nbsp;</td>
-						</tr>
-						<tr>
-							<td><h2><?PHP echo $EstartDate; ?><br><?= $row['EtimeStart'] ?> to <?= $row['EtimeEnd'] ?> </h2></td>
-							<td>&nbsp;</td>
-							<td colspan="7"><h4><?= $row['Edescription'] ?></h4></td>
-						</tr>
-						<tr>
-							<td rowspan="13"><img src= <?= $row['Eflyer'] ?> width="400px" height="300px"  ></td>
-							<td>&nbsp;</td>
-							<td>&nbsp;</td>
-							<td>&nbsp;</td>
-							<td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-							<td>&nbsp;</td>
-							<td>&nbsp;</td>
-							<td><a href="<?= $row['Ewebsite'] ?>" target="_blank"><?= $row['Ewebsite'] ?> </a></td>
-							<td>&nbsp;</td>
-							<td>&nbsp;</td>
-						</tr>
-						<tr>
-							<td>&nbsp;</td>
-							<td>&nbsp;</td>
-							<td><?PHP if ($row['Efacebook']){ ?>
+					<div class="content">
+						<input type="hidden" name="submitted" id="submitted" value="1" />
+						<input type="hidden" name="Eid" id="Eid" value="<?PHP echo $_GET['eid']; ?>" />
+						
+						<div><span class="error"><?php echo $fgmembersite->GetErrorMessage(); ?></span></div>
+					
+						<!--DASHBOARD-->
+						<div class="dashboard">
+							<div class="box">
+								<h1><?php echo $row['Evename']; ?></h1> 
+								<?PHP if($row['Ehashtag'] !== "" ){ ?> 
+									- <h1><?php echo $row['Ehashtag']; ?></h1>
+								<?PHP } ?>
+								
+								<h2><?PHP echo $row['EstartDate']; ?>&nbsp;
+								<?PHP echo date("H:i", strtotime($row['EtimeStart'])); ?>
+								to 
+								<?PHP echo date("H:i", strtotime($row['EtimeEnd'])); ?></h2>
+								<br>
+								
+								<h4><?PHP echo $row['Edescription']; ?></h4>
+								
+								<img src= <?= $row['Eflyer'] ?> alt="event image"/>
+								
+								<?PHP if($row['Ewebsite'] !== ""){?>
+									<a href="<?= $row['Ewebsite'] ?>" target="_blank">
+										<?PHP echo $row['Ewebsite']; ?>
+									</a>
+								<?PHP } ?>
+								
+								<?PHP if($row['Efacebook'] !== ""){ ?>
 									<a href="<?= $row['Efacebook'] ?>" target="_blank">
 										<img src="images/btn_fb.png" onMouseOver="this.src='images/btn_fbColor.png'" onMouseOut="this.src='images/btn_fb.png'" alt="Facebook" />
 									</a>
-								<?PHP } ?></td>
-							<td><?PHP if ($row['Etwitter']){ ?>
+								<?PHP } ?>
+								
+								<?PHP if ($row['Etwitter'] !== ""){ ?>
 									<a href="https://twitter.com/<?= $row['Etwitter'] ?>" target="_blank">
 										<img src="images/btn_twitter.png" onMouseOver="this.src='images/btn_twitterColor.png'" onMouseOut="this.src='images/btn_twitter.png'" alt="Twitter" />
 									</a>
-								<?PHP } ?></td>
-							<td><?PHP if ($row['Egoogle']){ ?>
+								<?PHP } ?>
+								
+								<?PHP if ($row['Egoogle'] !== ""){ ?>
 									<a href="<?= $row['Egoogle'] ?>" target="_blank">
 										<img src="images/btn_google.png" onMouseOver="this.src='images/btn_googleColor.png'" onMouseOut="this.src='images/btn_google.png'" alt="Google" />
 									</a>
-								<?PHP } ?></td>
-								<td><?PHP if ($row['Ehashtag']){ ?>
+								<?PHP } ?>
+								
+								<?PHP if ($row['Ehashtag'] !== ""){ ?>
 									<a href="https://instagram.com/<?= $row['Ehashtag'] ?>" target="_blank">
-										<img src="images/icon_instagram2.png" onMouseOver="this.src='images/icon_instagram2.png'" onMouseOut="this.src='images/icon_instagram2.png'" alt="Hashtag" />
+									<img src="images/icon_instagram2.png" onMouseOver="this.src='images/icon_instagram2.png'" onMouseOut="this.src='images/icon_instagram2.png'" alt="Hashtag" />
 									</a>
-								<?PHP } ?></td>
-						</tr>
-						<tr>
-							<td>&nbsp;</td>
-							<td>&nbsp;</td>
-							<td>&nbsp;</td>
-							<td>&nbsp;</td>
-							<td>&nbsp;</td>
-						</tr>
-						<tr>
-						<td rowspan="14"><!-- START OF MAP SCRIPT -->
-									<div id="map" style="width: 400px; height: 300px;"></div>
-									<script type="text/javascript" language= "php">
-										// Define your locations: HTML content for the info window, latitude, longitude
-										var locations = <?php echo json_encode($eventArray);?>;
+								<?PHP } ?>
+								
+								<!-- START OF MAP SCRIPT -->
+								<div id="map" style="width: 400px; height: 300px;"></div>
+								<script type="text/javascript" language= "php">
+									// Define your locations: HTML content for the info window, latitude, longitude
+									var locations = <?php echo json_encode($eventArray);?>;
 
-										// Setup the different icons and shadows
-										var iconURLPrefix = 'http://maps.google.com/mapfiles/ms/icons/';
+									// Setup the different icons and shadows
+									var iconURLPrefix = 'http://maps.google.com/mapfiles/ms/icons/';
 
-										var icons = ['images/favicon.png']
-										var icons_length = icons.length;
+									var icons = ['images/favicon.png']
+									var icons_length = icons.length;
 
-										var shadow = {
-											anchor: new google.maps.Point(15,33),
-											url: iconURLPrefix + 'msmarker.shadow.png'
-										};
+									var shadow = {
+									anchor: new google.maps.Point(15,33),
+									url: iconURLPrefix + 'msmarker.shadow.png'
+									};
 
-										var map = new google.maps.Map(document.getElementById('map'), {
-											zoom: 12,
-											center: new google.maps.LatLng(-50.92, 120.25),
-											mapTypeId: google.maps.MapTypeId.ROADMAP,
-											mapTypeControl: true,
-											streetViewControl: true,
-											panControl: true,
-											zoomControlOptions: {
-												position: google.maps.ControlPosition.LEFT_BOTTOM
-											}
-										});
+									var map = new google.maps.Map(document.getElementById('map'), {
+									zoom: 12,
+									center: new google.maps.LatLng(-50.92, 120.25),
+									mapTypeId: google.maps.MapTypeId.ROADMAP,
+									mapTypeControl: true,
+									streetViewControl: true,
+									panControl: true,
+									zoomControlOptions: {
+									position: google.maps.ControlPosition.LEFT_BOTTOM
+									}
+									});
 
-										var infowindow = new google.maps.InfoWindow({ maxWidth: 160 });
+									var infowindow = new google.maps.InfoWindow({ maxWidth: 160 });
 
-										var marker;
-										var markers = new Array();
+									var marker;
+									var markers = new Array();
 
-										var iconCounter = 0;
+									var iconCounter = 0;
 
-										// Add the markers and infowindows to the map
-										for (var i = 0; i < locations.length; i++){  
-											marker = new google.maps.Marker({
-												position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-												map: map,
-												icon : icons[iconCounter],
-												shadow: shadow
-											});
+									// Add the markers and infowindows to the map
+									for (var i = 0; i < locations.length; i++){  
+									marker = new google.maps.Marker({
+									position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+									map: map,
+									icon : icons[iconCounter],
+									shadow: shadow
+									});
 
-											markers.push(marker);
+									markers.push(marker);
 
-											google.maps.event.addListener(marker, 'click', (function(marker, i) {
-												return function() {
-													infowindow.setContent(locations[i][0]);
-													infowindow.open(map, marker);
-												}
-											})(marker, i));
+									google.maps.event.addListener(marker, 'click', (function(marker, i) {
+									return function() {
+									infowindow.setContent(locations[i][0]);
+									infowindow.open(map, marker);
+									}
+									})(marker, i));
 
-											iconCounter++;
-											// We only have a limited number of possible icon colors, so we may have to restart the counter
-											if(iconCounter >= icons_length){
-												iconCounter = 0;
-											}
-										}
+									iconCounter++;
+									// We only have a limited number of possible icon colors, so we may have to restart the counter
+									if(iconCounter >= icons_length){
+									iconCounter = 0;
+									}
+									}
 
-										function AutoCenter() {
-											//  Create a new viewpoint bound
-											var bounds = new google.maps.LatLngBounds();
-											//  Go through each...
-											$.each(markers, function (index, marker) {
-											bounds.extend(marker.position);
-											});
-											//  Fit these bounds to the map
-											map.fitBounds(bounds);
-										} AutoCenter();
-									</script> 
-									<!-- END OF MAP SCRIPT --></td>
-						  <td>&nbsp;</td>
-						  <td colspan="5"><h4><img src='images/favicon.png'>&nbsp;<?= $row['Eaddress'] ?>, <?= $row['Ecity'] ?>, <?= $row['Estate'] ?>, <?= $row['Ezip'] ?> </h4></td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td colspan="5"><h4>Phone: <?= $formatPhone?> </h4></td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-							
-							<td>&nbsp;</td>
-							<td>&nbsp;</td>
-							<td>&nbsp;</td>
-							<td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td></td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td></td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td></td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-						  <td>&nbsp;</td>
-						  <td>&nbsp;</td>
-						  <td></td>
-						  <td>&nbsp;</td>
-						</tr>
-						<tr>
-							<td></td>
-							<td>
+									function AutoCenter() {
+									//  Create a new viewpoint bound
+									var bounds = new google.maps.LatLngBounds();
+									//  Go through each...
+									$.each(markers, function (index, marker) {
+									bounds.extend(marker.position);
+									});
+									//  Fit these bounds to the map
+									map.fitBounds(bounds);
+									} AutoCenter();
+								</script> 
+								<!-- END OF MAP SCRIPT -->
+								
+								<h4>
+									<img src='images/favicon.png'/>&nbsp;
+									<?= $row['Eaddress'] ?>, 
+									<?= $row['Ecity'] ?>, 
+									<?= $row['Estate'] ?>, 
+									<?= $row['Ezip'] ?>
+								</h4>
+								
+								<?PHP echo $row['EphoneNumber']; ?>
+								
 								<?PHP if($fgmembersite->CheckSession() && ($usrname === $inDBUser)){ ?>
 									<input type="submit" name="submit" value="Delete Event"/>
 								<?PHP } ?>
-							</td>
-							<td></td>
-							<td>
+								
 								<?PHP if($fgmembersite->CheckSession() && ($usrname === $inDBUser)){ ?>
 									<a href="./editEvent.php?eid=<?PHP echo $newEventID; ?>"><img src="./images/btn_editevent.png"></a>
 								<?PHP } ?>
-							</td>
-						</tr>
-				<?PHP } ?>
-				</table>
+							</div>
+						</div><!--End of box-->
+					</div> <!-- End of content -->
+				<?php } ?>
 			</form>
 		</div>
-		<br/>
 		
-		<!-- SHOW SEARCH RESULTS -->
-		<div class="events" id="txtHint"></div>
-
 		<div class="links">
 			<?PHP include './links.php'; ?>
 		</div>
-
+		
 		<div class="footer">
 			<?PHP include './footer.php'; ?>
 		</div>
