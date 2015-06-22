5b6512a8a18ce9f1bac275cd3d35892a0375fbb1
flake79936 <robert.madrid14@gmail.com>
2015-06-22 17:12:04 -0600

    RM
    temp paypal files

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$PayPalMode         = 'sandbox'; // sandbox or live
+$PayPalApiUsername  = 'selleremail@somesite.com'; //PayPal API Username
+$PayPalApiPassword  = '123456'; //Paypal API password
+$PayPalApiSignature     = 'ZWxwchnCsdQg5PxAUjcH6OPuZK3sPcPH'; //Paypal API Signature
+$PayPalCurrencyCode     = 'USD'; //Paypal Currency Code
+$PayPalReturnURL    = 'http://yoursite.com/paypal/process.php'; //Point to process.php page
+$PayPalCancelURL    = 'http://yoursite.com/paypal/cancel_url.php'; //Cancel URL if user clicks cancel
+?>
---
+++ product.php
@@ -0,0 +1,19 @@
+<?php
+include_once("config.php");
+?>
+<h2 align="center">Test Products</h2>
+<table class="procut_item" border="0" cellpadding="4">
+  <tr>
+    <td width="70%"><h4>Product 1</h4>(product description)</td>
+    <td width="30%">
+    <form method="post" action="process.php">
+    <input type="hidden" name="itemname" value="Product 1" /> 
+    <input type="hidden" name="itemnumber" value="10000" /> 
+    <input type="hidden" name="itemdesc" value="product description." /> 
+    <input type="hidden" name="itemprice" value="225.00" />
+    Quantity : <select name="itemQty"><option value="1">1</option><option value="2">2</option><option value="3">3</option></select> 
+    <input class="dw_button" type="submit" name="submitbutt" value="Buy (225.00 <?php echo $PayPalCurrencyCode; ?>)" />
+    </form>
+    </td>
+  </tr>
+</table>
