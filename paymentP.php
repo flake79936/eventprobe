68ca4af4c88204ae221ee9a2ec6ddaaee4c9130d
flake79936 <robert.madrid14@gmail.com>
2015-08-05 21:09:20 -0600

    RM

--- paymentP.php
+++ paymentP.php
@@ -1,36 +1,42 @@
-<!–Paypal Form starts–>
-<form action=”https://www.sandbox.paypal.com/cgi-bin/webscr” method=”post” >
-
-<!– Identify your business so that you can collect the payments. –>
-<input type=”hidden” name=”business” value=”noemaildavis-facilitator@gmail.com”>
-
-<!– Specify a Buy Now button. –>
-<input type=”hidden” name=”cmd” value=”_xclick”>
-
-<!– Specify details about the item that buyers will purchase. –>
-
-<--<input type=”hidden” name=”item_name” value=”<?php echo $product_name;?>”> -->
-<input type=”hidden” name=”item_name” value=”Event_name>”>
-
-<input type=”hidden” name=”item_number” value=”<?php echo $Eid; ?>">
+<html>
+<!-- Paypal Form starts -->
+<!-- <form action=”https://www.sandbox.paypal.com/cgi-bin/webscr” method=”post” > -->
+<form id="eventForm" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" >
 
 
-<input type=”hidden” name=”amount” value=”10”>
-<--<input type=”hidden” name=”amount” value=”<?php echo $product_price;?>”>-->
+<!--  Identify your business so that you can collect the payments.  -->
 
-<input type=”hidden” name=”quantity” value=”1”>
-<input type=”hidden” name=”currency_code” value=”USD”>
+<input type="hidden" name="business" value="noemaildavis-facilitator@gmail.com"/>
 
-<–Specify the Pages for Successful payment & failed Payment–>
+<!-- Specify a Buy Now button. -->
 
-<input type=”hidden” name=”return” value=”http://www.eventprobe.com/paypal/payment_success2.php”/>
+<input type="hidden" name="cmd" value="_xclick"/>
 
-<input type=”hidden” name=”cancel_return” value=”http://www.eventprobe.com/paypal/payment_cancel2.php”/>
+<!-- Specify details about the item that buyers will purchase. -->
 
-<!– Display the payment button. –>
-<input type=”image” name=”submit” border=”0″ src=”paypal_button.png” alt=”PayPal – The safer, easier way to pay online”>
-<img alt=”” border=”0″ width=”1″ height=”1″ src=”https://www.paypalobjects.com/en_US/i/scr/pixel.gif” >
+<!-- <input type=”hidden” name=”item_name” value=”<?php echo $product_name;?>”>  -->
+<input type="hidden" name="item_name" value="event name"/>
+
+<!-- <input type=”hidden” name=”item_number” value=”<?php echo $Eid; ?>"> -->
+
+
+<input type="hidden" name="amount" value="10"/>
+<!-- <input type=”hidden” name=”amount” value=”<?php echo $product_price;?>”> -->
+
+<input type="hidden" name="quantity" value="1"/>
+<input type="hidden" name="currency_code" value="USD"/>
+
+<!-- Specify the Pages for Successful payment & failed Payment -->
+
+<input type="hidden" name="return" value="http://www.eventprobe.com/paypal/payment_success2.php"/>
+
+<input type="hidden" name="cancel_return" value="http://www.eventprobe.com/paypal/payment_cancel2.php"/>
+
+<!-- Display the payment button. -->
+<input type="image" name="submit" border="0" src="paypal_button.png" alt="PayPal – The safer, easier way to pay online">
+
+<img width="200px" height="200px" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" alt="Paypal" />
 
 </form>
-
-<–Paypal Form ends–>
+</html>
+<!-- Paypal Form ends-->
---
+++ paypal/payment_success2.php
@@ -0,0 +1,63 @@
+<?PHP 
+include './dbconnect.php';
+?>
+<!DOCTYPE html><html>
+<head>
+<title>Payment Successful!</title>
+</head>
+<body>
+
+<?php
+
+//Getting payment details from paypal
+
+$Eid = $_GET['item_number'];
+$amount = $_GET['amt'];
+$currency = $_GET['mc_currency'];
+$trx_id = $_GET['tx'];
+$status = $_GET['payment_status'];
+$email = $_GET['receiver_email'];
+$payment_fee = $_GET['payment_fee'];
+$payment_gross = $_GET['payment_gross'];
+
+
+
+
+$total=$payment_fee+$payment_gross;
+//inserting the payment to table
+
+$sql = "insert into payment values ('".$Eid."','".$total."','".$currency."' ,'".$trx_id."' ";
+echo $sql;
+$result = mysqli_query($con, $sql);
+echo$result;
+// if($currency=="USD" && $amount==10){
+?>
+
+ <h2>Welcome,Your Payment was successful!</h2>
+<?PHP
+echo "start";
+echo $Eid;
+echo $amount;
+echo $currency;
+echo $trx_id;
+echo $status ;
+echo $email;
+echo $payment_fee;
+echo $payment_gross;
+echo "end";
+
+
+ 
+?>
+<?PHP
+// }
+// else {
+?>
+<h2>Welcome Guest, Payment was failed</h2><br>
+<a href="http://www.eventprobe.com">Go Back to eventprobe</a>
+<?PHP
+// }
+
+?>
+</body>
+</html>
