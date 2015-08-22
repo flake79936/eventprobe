a59d31f8b070c4b540c15c321fcebe69c8007e6e
flake79936 <robert.madrid14@gmail.com>
2015-08-22 15:36:07 -0600

    RM

--- payment_success2.php
+++ payment_success2.php
@@ -42,23 +42,27 @@ $completed="Completed";
 
 
 //Getting payment details from paypal
-$custom = $_POST['custom'];
-$EidP = $_POST['item_number'];
-$name = $_POST['item_name'];
-$currency = $_POST['mc_currency'];
-$trx_id = $_POST['txn_id'];
-$status = $_POST['payment_status'];
-$email = $_POST['receiver_email'];
-$payment_fee = $_POST['payment_fee'];
-$payment_gross = $_POST['payment_gross'];
+if (isset($_POST['custom'])) {$custom = $_POST['custom'];}
+if (isset($_POST['item_number'])) {$EidP = $_POST['item_number'];}else{$EidP = $_GET['item_number'];}
+if (isset($_POST['item_name'])) {$name = $_POST['item_name'];}
+if (isset($_POST['mc_currency'])) {$currency = $_POST['mc_currency'];}else{$currency = $_GET['cc'];}
+if (isset($_POST['payment_status'])) {$status = $_POST['payment_status'];}else{$status = $_GET['st'];}
+if (isset($_POST['receiver_email'])) {$email = $_POST['receiver_email'];}else{$email = $_GET['cm'];}
+if (isset($_POST['payment_fee'])) {$payment_fee = $_POST['payment_fee'];}
+if (isset($_POST['payment_gross'])) {$payment_gross = $_POST['payment_gross'];}else{$payment_gross = $_GET['amt'];}
+if (isset($_POST['txn_id'])) {$trx_id = $_POST['txn_id'];}else{$trx_id = $_GET['tx'];}
 
 
-$unique="SELECT *  FROM payment WHERE PtrxN_id= ".$trx_id."";
-$zero= 0;
+// $custom = $_POST['custom'];
+// $EidP = $_POST['item_number'];
+// $name = $_POST['item_name'];
+// $currency = $_POST['mc_currency'];
+// $trx_id = $_POST['txn_id'];
+// $status = $_POST['payment_status'];
+// $email = $_POST['receiver_email'];
+// $payment_fee = $_POST['payment_fee'];
+// $payment_gross = $_POST['payment_gross'];
 
-
-$result = mysqli_query($con,$unique);
-$num_rows = mysql_num_rows($result);
 
 
 $total=$payment_gross;
@@ -68,6 +72,11 @@ $ErankP= "Premium";
  }else{
 $ErankP= "Paid";
  }
+
+
+
+
+
 
 $item_name = $con->query("SELECT Evename FROM Events WHERE Eid = ".$EidP."")->fetch_row()[0];
 $Erank= $con->query("SELECT Erank FROM Events WHERE Eid = ".$EidP."")->fetch_row()[0];
@@ -95,7 +104,7 @@ if (mysqli_query($con, $sql2)) {
 $sql = "INSERT INTO payment ( Eid , Pamount , Pcurrency , Ptrxn_id ) VALUES ( '".$EidP."' , '".$total."' , '".$currency."' , '".$trx_id."' ) ";
 
 if (mysqli_query($con, $sql)) {
-    echo "Thank you for your Payment";
+    echo "Thank you for your payment. Your transaction has been completed, and a receipt for your purchase has been emailed to you. You may log into your account at www.sandbox.paypal.com/us to view details of this transaction.";
     ?>
     <br>
     <?PHP
