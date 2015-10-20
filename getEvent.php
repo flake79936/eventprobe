c8bc2fb30b5c03902d42430f40c8c1e80085e836
flake79936 <robert.madrid14@gmail.com>
2015-10-19 20:55:29 -0600

    RM

--- getEvent.php
+++ getEvent.php
@@ -88,6 +88,77 @@
 				
 				//echo $var . " variable <br>";
 				
+				
+			function state_abbr($name, $get = 'abbr') {
+			//make sure the state name has correct capitalization:
+				if($get != 'name') {
+				$name = strtolower($name);
+				$name = ucwords($name);
+				}else{
+				$name = strtoupper($name);
+				}
+					$states = array(
+					'Alabama'=>'AL',
+					'Alaska'=>'AK',
+					'Arizona'=>'AZ',
+					'Arkansas'=>'AR',
+					'California'=>'CA',
+					'Colorado'=>'CO',
+					'Connecticut'=>'CT',
+					'Delaware'=>'DE',
+					'Florida'=>'FL',
+					'Georgia'=>'GA',
+					'Hawaii'=>'HI',
+					'Idaho'=>'ID',
+					'Illinois'=>'IL',
+					'Indiana'=>'IN',
+					'Iowa'=>'IA',
+					'Kansas'=>'KS',
+					'Kentucky'=>'KY',
+					'Louisiana'=>'LA',
+					'Maine'=>'ME',
+					'Maryland'=>'MD',
+					'Massachusetts'=>'MA',
+					'Michigan'=>'MI',
+					'Minnesota'=>'MN',
+					'Mississippi'=>'MS',
+					'Missouri'=>'MO',
+					'Montana'=>'MT',
+					'Nebraska'=>'NE',
+					'Nevada'=>'NV',
+					'New Hampshire'=>'NH',
+					'New Jersey'=>'NJ',
+					'New Mexico'=>'NM',
+					'New York'=>'NY',
+					'North Carolina'=>'NC',
+					'North Dakota'=>'ND',
+					'Ohio'=>'OH',
+					'Oklahoma'=>'OK',
+					'Oregon'=>'OR',
+					'Pennsylvania'=>'PA',
+					'Rhode Island'=>'RI',
+					'South Carolina'=>'SC',
+					'South Dakota'=>'SD',
+					'Tennessee'=>'TN',
+					'Texas'=>'TX',
+					'Utah'=>'UT',
+					'Vermont'=>'VT',
+					'Virginia'=>'VA',
+					'Washington'=>'WA',
+					'West Virginia'=>'WV',
+					'Wisconsin'=>'WI',
+					'Wyoming'=>'WY'
+					);
+				if($get == 'name') {
+				// in this case $name is actually the abbreviation of the state name and you want the full name
+			$states = array_flip($states);
+				}
+
+			return $states[$name];
+			}
+				$state=$var;
+				$var = state_abbr($state);
+				
 				$var = "'.*" . $var . ".*'";
 				$qry = "SELECT * FROM Events ";
 				$qry .= $var != null ? 
